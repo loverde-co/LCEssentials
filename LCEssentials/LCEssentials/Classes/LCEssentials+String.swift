e62c786a40b7e3721bb373153bee6da5f2f475b6
Daniel Arantes Loverde <daniel@loverde.com.br>
2018-07-28 13:59:32 -0300

    Currency formatter when typing

--- LCEssentials/LCEssentials/Classes/LCEssentials+String.swift
+++ LCEssentials/LCEssentials/Classes/LCEssentials+String.swift
@@ -296,6 +296,34 @@ public extension String {
         return priceString!
     }
     
+    public func currencyInputFormatting(withSpace: Bool = false) -> String {
+        
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = NumberFormatter.Style.currency
+        formatter.currencySymbol = withSpace ? "\(formatter.currencySymbol!) " : formatter.currencySymbol
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        formatter.locale = NSLocale.current
+        
+        var amountWithPrefix = self
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+        
+        return formatter.string(from: number)!
+    }
+    
     
     public func convertFloatToCurrency(value: Float) -> String{
 
