520446ae15e455aef9bf57fec4101b6ddd658054
Daniel Arantes Loverde <daniel@loverde.com.br>
2020-06-02 17:17:37 -0300

    Features: Add image load async with cache

--- LCEssentials/Classes/LCEssentials+UIImageView.swift
+++ LCEssentials/Classes/LCEssentials+UIImageView.swift
@@ -57,6 +57,89 @@ extension UIImageView {
             completion(true)
         }
     }
+    
+    private static var taskKey = 0
+    private static var urlKey = 0
+
+    private var currentTask: URLSessionTask? {
+        get { return objc_getAssociatedObject(self, &UIImageView.taskKey) as? URLSessionTask }
+        set { objc_setAssociatedObject(self, &UIImageView.taskKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+
+    private var currentURL: URL? {
+        get { return objc_getAssociatedObject(self, &UIImageView.urlKey) as? URL }
+        set { objc_setAssociatedObject(self, &UIImageView.urlKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+
+    func donwloadImageAsync(with urlString: String?, alternativeImage: UIImage, completion: (() -> Void)? = nil) {
+        // cancel prior task, if any
+
+        weak var oldTask = currentTask
+        currentTask = nil
+        oldTask?.cancel()
+
+        // reset imageview's image
+
+        self.image = alternativeImage
+
+        // allow supplying of `nil` to remove old image and then return immediately
+
+        guard let urlString = urlString else { return }
+
+        // check cache
+
+        if let cachedImage = ImageCache.shared.image(forKey: urlString) {
+            self.image = cachedImage
+            completion?()
+            return
+        }
+
+        // download
+
+        let url = URL(string: urlString)!
+        currentURL = url
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            self?.currentTask = nil
+
+            //error handling
+
+            if let error = error {
+                // don't bother reporting cancelation errors
+                self?.image = alternativeImage
+                if (error as NSError).domain == NSURLErrorDomain && (error as NSError).code == NSURLErrorCancelled {
+                    completion?()
+                    return
+                }
+                
+                printError(title: "loadImageAsync", msg: error.localizedDescription)
+                completion?()
+                return
+            }
+
+            guard let data = data, let downloadedImage = UIImage(data: data) else {
+                printError(title: "loadImageAsync", msg: "unable to extract image")
+                completion?()
+                return
+            }
+
+            ImageCache.shared.save(image: downloadedImage, forKey: urlString)
+
+            if url == self?.currentURL {
+                DispatchQueue.main.async {
+                    self?.image = downloadedImage
+                    completion?()
+                }
+            }
+        }
+
+        // save and start new task
+
+        currentTask = task
+        task.resume()
+    }
+    
+    
+    
 
     @IBInspectable open var imageColor: UIColor! {
         set {
@@ -86,6 +169,33 @@ extension UIImageView {
         let imgView = self
         imgView.blur(withStyle: style)
         return imgView
+    }
+}
+
+class ImageCache {
+    private let cache = NSCache<NSString, UIImage>()
+    private var observer: NSObjectProtocol!
+
+    static let shared = ImageCache()
+
+    private init() {
+        // make sure to purge cache on memory pressure
+
+        observer = NotificationCenter.default.addObserver(forName: .UIApplicationDidReceiveMemoryWarning, object: nil, queue: nil) { [weak self] notification in
+            self?.cache.removeAllObjects()
+        }
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(observer)
+    }
+
+    func image(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+
+    func save(image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
     }
 }
 #endif
