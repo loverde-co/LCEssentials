1992672d77b7ca0fb528d2958371621d683faa48
Daniel Arantes Loverde <daniel@loverde.com.br>
2024-10-15 15:20:34 -0300

    SPM update

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LCEssentials",
-            dependencies: [])
+            dependencies: [],
+            exclude:["PickerViews/*", "PickerViews/Storyboards/*"])
     ]
 )
