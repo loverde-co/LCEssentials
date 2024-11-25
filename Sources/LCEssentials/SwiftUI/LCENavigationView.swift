433f87be3f8ffcaafa023a8722765df4af893ca7
Developer @ Loverde Company <developer@loverde.com.br>
2024-11-25 14:25:51 -0300

    fix: LCENavigationView for SwiftUI

--- Sources/LCEssentials/SwiftUI/LCENavigationView.swift
+++ Sources/LCEssentials/SwiftUI/LCENavigationView.swift
@@ -23,11 +23,11 @@ import SwiftUI
 
 @available(iOS 15, *)
 class LCENavigationState: ObservableObject {
-    @Published var rightButtonImage: Image? = nil
+    @Published var rightButtonImage: AnyView? = nil
     @Published var rightButtonText: Text = Text("")
     @Published var rightButtonAction: () -> Void = {}
     
-    @Published var leftButtonImage: Image? = nil
+    @Published var leftButtonImage: AnyView? = nil
     @Published var leftButtonText: Text = Text("")
     @Published var leftButtonAction: () -> Void = {}
     
@@ -65,11 +65,7 @@ public struct LCENavigationView<Content: View>: View {
         }
         .navigationBarHidden(true)
     }
-}
 
-@available(iOS 15, *)
-public extension LCENavigationView {
-    
     private var NavigationBarView: some View {
         HStack {
             NavLeftButton
@@ -96,49 +92,185 @@ public extension LCENavigationView {
     
     private var NavLeftButton: some View {
         Button(action: state.leftButtonAction) {
-            if let image = state.leftButtonImage {
-                image
+            HStack {
+                if let image = state.leftButtonImage {
+                    image
+                }
+                state.leftButtonText
             }
-            state.leftButtonText
         }
     }
     
     private var NavRightButton: some View {
         Button(action: state.rightButtonAction) {
-            if let image = state.rightButtonImage {
-                image
+            HStack {
+                state.rightButtonText
+                if let image = state.rightButtonImage {
+                    image
+                }
             }
-            state.rightButtonText
         }
     }
     
-    func setRightButton(text: Text,
-                        image: Image? = nil,
-                        action: @escaping () -> Void) -> LCENavigationView {
-        state.rightButtonImage = image
-        state.rightButtonText = text
+    func setRightButton(
+        text: String,
+        color: Color = .primary,
+        image: Image? = nil,
+        action: @escaping () -> Void
+    ) -> Self {
+        if let image {
+            state.rightButtonImage = AnyView(image)
+        } else {
+            state.rightButtonImage = nil
+        }
+        state.rightButtonText = Text(text).foregroundColor(color)
         state.rightButtonAction = action
+        
+        if let string = state.leftButtonText.string, string.isEmpty {
+            state.leftButtonText = Text(text).foregroundColor(.clear)
+        }
+        if state.leftButtonImage == nil {
+            state.leftButtonImage = image?.foregroundColor(.clear) as? AnyView
+        }
+        
         return self
     }
     
-    func setLeftButton(text: Text,
-                       image: Image? = nil,
-                       action: @escaping () -> Void) -> LCENavigationView {
-        state.leftButtonImage = image
-        state.leftButtonText = text
+    func setLeftButton(
+        text: String,
+        color: Color = .primary,
+        image: Image? = nil,
+        action: @escaping () -> Void
+    ) -> Self {
+        if let image {
+            state.leftButtonImage = AnyView(image)
+        } else {
+            state.leftButtonImage = nil
+        }
+        state.leftButtonText = Text(text).foregroundColor(color)
         state.leftButtonAction = action
+        
+        if let string = state.rightButtonText.string, string.isEmpty {
+            state.rightButtonText = Text(text).foregroundColor(.clear)
+        }
+        if state.rightButtonImage == nil {
+            state.rightButtonImage = image?.foregroundColor(.clear) as? AnyView
+        }
+        
         return self
     }
     
-    func setTitle(text: Text, subTitle: Text? = nil) -> LCENavigationView {
+    func setTitle(
+        text: Text,
+        subTitle: Text? = nil
+    ) -> Self {
         state.title = text
         state.subTitle = subTitle
         return self
     }
     
-    func hideNavigationView(_ hide: Bool) -> LCENavigationView {
+    func hideNavigationView(_ hide: Bool) -> Self {
         state.hideNavigationBar = hide
         return self
     }
 }
+
+@available(iOS 15.0, *)
+extension FormatStyle {
+    func format(any value: Any) -> FormatOutput? {
+        if let v = value as? FormatInput {
+            return format(v)
+        }
+        return nil
+    }
+}
+
+@available(iOS 15.0, *)
+extension LocalizedStringKey {
+    var resolved: String? {
+        let mirror = Mirror(reflecting: self)
+        guard let key = mirror.descendant("key") as? String else {
+            return nil
+        }
+
+        guard let args = mirror.descendant("arguments") as? [Any] else {
+            return nil
+        }
+
+        let values = args.map { arg -> Any? in
+            let mirror = Mirror(reflecting: arg)
+            if let value = mirror.descendant("storage", "value", ".0") {
+                return value
+            }
+
+            guard let format = mirror.descendant("storage", "formatStyleValue", "format") as? any FormatStyle,
+                  let input = mirror.descendant("storage", "formatStyleValue", "input") else {
+                return nil
+            }
+
+            return format.format(any: input)
+        }
+
+        let va = values.compactMap { arg -> CVarArg? in
+            switch arg {
+            case let i as Int:      return i
+            case let i as Int64:    return i
+            case let i as Int8:     return i
+            case let i as Int16:    return i
+            case let i as Int32:    return i
+            case let u as UInt:     return u
+            case let u as UInt64:   return u
+            case let u as UInt8:    return u
+            case let u as UInt16:   return u
+            case let u as UInt32:   return u
+            case let f as Float:    return f
+            case let f as CGFloat:  return f
+            case let d as Double:   return d
+            case let o as NSObject: return o
+            default:                return nil
+            }
+        }
+
+        if va.count != values.count {
+            return nil
+        }
+
+        return String.localizedStringWithFormat(key, va)
+    }
+}
+
+@available(iOS 15.0, *)
+extension Text {
+    var string: String? {
+        let mirror = Mirror(reflecting: self)
+        if let s = mirror.descendant("storage", "verbatim") as? String {
+            return s
+        } else if let attrStr = mirror.descendant("storage", "anyTextStorage", "str") as? AttributedString {
+            return String(attrStr.characters)
+        } else if let key = mirror.descendant("storage", "anyTextStorage", "key") as? LocalizedStringKey {
+            return key.resolved
+        } else if let format = mirror.descendant("storage", "anyTextStorage", "storage", "format") as? any FormatStyle,
+                  let input = mirror.descendant("storage", "anyTextStorage", "storage", "input") {
+            return format.format(any: input) as? String
+        } else if let formatter = mirror.descendant("storage", "anyTextStorage", "formatter") as? Formatter,
+                  let object = mirror.descendant("storage", "anyTextStorage", "object") {
+            return formatter.string(for: object)
+        }
+        return nil
+    }
+}
+
+//@available(iOS 15.0, *)
+//struct LCENavigationView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LCENavigationView {
+//            Text("Hello, World!")
+//        }
+//        .setTitle(text: Text("Hellow Nav Title"))
+//        .setLeftButton(text: "Back") {
+//            print("Button tapped!")
+//        }
+//    }
+//}
+
 #endif
