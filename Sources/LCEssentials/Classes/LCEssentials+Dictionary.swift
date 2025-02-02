4f00300608c4a1571abd1cad8fd99e10c6b2315e
Daniel Arantes Loverde <daniel@loverde.com.br>
2025-02-02 10:09:43 -0300

    missing detail

--- Sources/LCEssentials/Classes/LCEssentials+Dictionary.swift
+++ Sources/LCEssentials/Classes/LCEssentials+Dictionary.swift
@@ -316,7 +316,7 @@ public extension Dictionary where Key: StringProtocol {
     }
 }
 
-extension Dictionary where Value: Hashable {
+public extension Dictionary where Value: Hashable {
     var uniqueValues: Dictionary<Key, Value> {
         var seenValues = Set<Value>()
         return self.filter { seenValues.insert($0.value).inserted }
